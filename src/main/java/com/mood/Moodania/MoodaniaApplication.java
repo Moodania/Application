897dd1c8555655123e6679e3a29e497be6093200
NaMe2te <vladyushashishkin81@gmail.com>
2023-04-18 22:06:24 +0300

    init

---
+++ src/main/java/com/mood/Moodania/MoodaniaApplication.java
@@ -0,0 +1,12 @@
+package com.mood.Moodania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodaniaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MoodaniaApplication.class, args);
+	}
+
+}
